2a63ecf6ef2be1b4b22cc1b2fbad399746272efc
manu-git-hub <ms.manu.856@gmail.com>
2025-11-13 02:03:33 +0530

    ints

--- variables/main.go
+++ variables/main.go
@@ -15,4 +15,20 @@ func main() {
 
 	nameFour := "yoshi"
 	fmt.Println(nameFour)
+
+	//ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits & memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint8 = 25
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 25000000000000000000.5
+	scoreThree := 25.98
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
 }
